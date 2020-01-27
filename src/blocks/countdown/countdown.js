31087827eb9f354ed04fe0d24aeef5b303083125
des-yogi <nuevo-design@mail.ru>
2020-01-27 19:44:49 +0200

    in progress

--- src/blocks/countdown/countdown.js
+++ src/blocks/countdown/countdown.js
@@ -1,21 +1,10 @@
 $( document ).ready(function() {
-  var dateContainers = document.querySelectorAll('.date--time-left');
-
-  for(var i=0; i < dateContainers.length; i++) {
-    var finalDate = '00/00/00 23:59:59';
-    var dateContainer = dateContainers[i];
-    console.log( $('.countdown', dateContainer) );
-
-    if (dateContainer && dateContainer.dataset.countdown) {
-      finalDate = dateContainer.dataset.countdown;
-
-      $( '.countdown', dateContainer ).downCount({
-        date: finalDate, //формат m:d:Y
-        offset: +2 // зимнее время +2, летнее +3
-      }, function () {
-        //alert('Время истекло!');
-      });
-    }
-  }
+  var finalDate = '03/01/2020 23:59:59';
+  $( '.countdown' ).downCount({
+    date: finalDate, //формат m:d:Y
+    offset: +2 // зимнее время +2, летнее +3
+  }, function () {
+    //alert('Время истекло!');
+  });
 
 });
